c530e977b676025acd69439c266ded3f9e9e79f5
Raphael Costa <rapha2706@hotmail.com>
2019-11-14 11:19:06 +0000

    adding clean data and data source

---
+++ InsertData.sql
@@ -0,0 +1,8 @@
+INSERT INTO TABLE atp (
+  ATP, Location, Tournament, Date, Series, Court, Surface,Round,
+  Best of, Winner, Loser, WRank, LRank, WPts, LPts, W1, L1, 
+  W2, L2, W3, L3, W4, L4, W5, L5, Wsets, Lsets, Comment, 
+  B365W, B365L, EXW, EXL, LBW, LBL, PSW, PSL, MaxW, MaxL, AvgW, AvgL) 
+VALUES(
+    
+);
